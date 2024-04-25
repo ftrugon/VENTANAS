e678001c37b00a99b70883562435969e54f9c5a3
ftrugon <fjtrujillo@iesrafaelalberti.es>
2024-04-25 21:05:04 +0200

    hecho :d

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,67 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.res.useResource
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+
+fun main() = application {
+    val icon = BitmapPainter(useResource("sample.png", ::loadImageBitmap))
+    val mainWindowState = rememberWindowState()
+    var showMainWindow by remember { mutableStateOf(true) }
+    var showSecondWindow by remember { mutableStateOf(false) }
+
+    if (showMainWindow) {
+        MainWindow(icon,mainWindowState,{showMainWindow = false},{ showMainWindow = false;showSecondWindow = true})
+    }
+
+    if (showSecondWindow) {
+        SecondaryWindow(icon, mainWindowState,{ showSecondWindow = false },{showMainWindow = true; showSecondWindow = false})
+    }
+
+    if (!showMainWindow && !showSecondWindow) {
+        exitApplication()
+    }
+}
 
 @Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+fun MainWindow(
+    icon:BitmapPainter,
+    windowState: WindowState,
+    onClose: () -> Unit,
+    onClickSecondWindow: () -> Unit
+){
+    Window(
+        onCloseRequest = onClose,
+        title = "Ventana Principal",
+        icon = icon,
+        state = windowState
+    ) {
+        Button(onClick = onClickSecondWindow) {
+            Text("Abrir Ventana Secundaria y cerrar esta")
         }
     }
 }
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+@Composable
+fun SecondaryWindow(
+    icon:BitmapPainter,
+    windowState: WindowState,
+    onClose: () -> Unit,
+    onClickSecondWindow: () -> Unit
+){
+    Window(
+        onCloseRequest = onClose,
+        title = "Ventana Secundaria",
+        icon = icon,
+        state = windowState
+    ) {
+        Button(onClick = onClickSecondWindow) {
+            Text("Abrir Ventana Principal y cerrar esta")
+        }
     }
 }
